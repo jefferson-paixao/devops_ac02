c27ee5bd8def1c33227c2c7426babb9c1d1006c8
Jefferson <jefferson.paixao@aluno.faculdadeimpacta.com.br>
2020-04-03 23:10:25 -0300

    Arrumando o nome da funcao de teste

--- com/jefferson/test/test_operacoes.py
+++ com/jefferson/test/test_operacoes.py
@@ -5,5 +5,5 @@ class TestOperacoes(TestCase):
     def setUp(self):
         self.operacoes = Operacoes()
 
-    def test_soma(self):
+    def test_multiplicacao(self):
         self.assertEqual(self.operacoes.multiplicacao(5,5),25,"Deveria ser 25!")
